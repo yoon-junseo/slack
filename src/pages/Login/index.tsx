995a459fc99556945fb89a992c885020bbd94b7b
junseo <ysj6844@naver.com>
2021-07-07 00:20:01 +0900

    feature: 로그인, 회원가입 로그인, 회원가입 페이지 UI 구현 #1

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,11 +1,40 @@
-import React from 'react';
+import React, { useCallback, useState } from 'react';
+import { Header, Form, Label, Input, Error, Button, LinkContainer } from '@pages/SignUp/style';
+import { Link } from 'react-router-dom';
 
 const Login = () => {
-    return (
-        <div>
-            Login
-        </div>
-    );
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [logInError, setLoginError] = useState('');
+
+  const onChangeEmail = useCallback(() => {}, []);
+  const onChangePassword = useCallback(() => {}, []);
+
+  return (
+    <div id="container">
+      <Header>Sleact</Header>
+      <Form>
+        <Label id="email-label">
+          <span>이메일 주소</span>
+          <div>
+            <Input type="email" id="email" name="email" value={email} onChange={onChangeEmail} />
+          </div>
+        </Label>
+        <Label id="password-label">
+          <span>비밀번호</span>
+          <div>
+            <Input type="password" id="password" name="password" value={password} onChange={onChangePassword} />
+          </div>
+          {logInError && <Error>이메일과 비밀번호 조합이 일치하지 않습니다.</Error>}
+        </Label>
+        <Button type="submit">로그인</Button>
+      </Form>
+      <LinkContainer>
+        아직 회원이 아니신가요?&nbsp;
+        <Link to="/signup">회원가입 하러가기</Link>
+      </LinkContainer>
+    </div>
+  );
 };
 
 export default Login;
