6ba893cfafa1bc436db69c1ba716f1cbf37194b2
junseo <ysj6844@naver.com>
2021-07-13 22:57:32 +0900

    chore: 배포 확인

--- router/Router.tsx
+++ router/Router.tsx
@@ -2,13 +2,15 @@ import React from 'react';
 import loadable from '@loadable/component';
 import { Switch, Route, Redirect } from 'react-router';
 
+const Home = loadable(() => import('@pages/Home/Home'));
 const LogIn = loadable(() => import('@pages/Login'));
 const SignUp = loadable(() => import('@pages/SignUp'));
 
 const Router = () => {
   return (
     <Switch>
-      <Redirect exact path="/" to="/login" />
+      {/* <Redirect exact path="/" to="/login" /> */}
+      <Route path="/" component={Home} exact />
       <Route path="/login" component={LogIn} />
       <Route path="/signup" component={SignUp} />
     </Switch>
