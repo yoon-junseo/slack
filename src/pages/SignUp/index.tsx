48c9e70514a804ec06ea206e7580d745bc370fa6
junseo <ysj6844@naver.com>
2021-07-26 23:56:51 +0900

    Feature: 회원가입 페이지 1. UI 구현 #3

--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -1,63 +1,82 @@
 import React, { useCallback, useState } from 'react';
-import { Header, Form, Label, Input, Error, Button, LinkContainer } from './style';
 import { Link } from 'react-router-dom';
+import { Form, Label, Input, LinkContainer, Button, Header, Error } from './styles';
+import useInput from '@hooks/useInput';
 
 const SignUp = () => {
-  const [email] = useState('');
-  const [nickname] = useState('');
-  const [password] = useState('');
-  const [passwordCheck] = useState('');
+  const [email, onChangeEmail] = useInput('');
+  const [nickname, onChangeNickname] = useInput('');
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+  const [mismatchError, setMismatchError] = useState(false);
 
-  const onChangeEmail = useCallback(() => {}, []);
-  const onChangeNickname = useCallback(() => {}, []);
-  const onChangePassword = useCallback(() => {}, []);
-  const onChangePasswordCheck = useCallback(() => {}, []);
-  const onSubmit = useCallback(() => {}, []);
+  const onChangePassword = useCallback(
+    (e) => {
+      setPassword(e.target.value);
+      setMismatchError(e.target.value !== confirmPassword);
+    },
+    [confirmPassword],
+  );
 
+  const onChangeConfirmPassword = useCallback(
+    (e) => {
+      setConfirmPassword(e.target.value);
+      setMismatchError(e.target.value !== password);
+    },
+    [password],
+  );
+
+  const onSubmit = useCallback(
+    (e) => {
+      e.preventDefault();
+      if (!mismatchError) {
+        console.log('회원가입');
+      }
+    },
+    [email, nickname, password, confirmPassword, mismatchError],
+  );
   return (
     <div id="container">
       <Header>Sleact</Header>
       <Form onSubmit={onSubmit}>
-        <Label id="email-label">
+        <Label htmlFor="email" id="email-label">
           <span>이메일 주소</span>
           <div>
             <Input type="email" id="email" name="email" value={email} onChange={onChangeEmail} />
           </div>
         </Label>
-        <Label id="nickname-label">
+        <Label htmlFor="nickname" id="nickname-label">
           <span>닉네임</span>
           <div>
             <Input type="text" id="nickname" name="nickname" value={nickname} onChange={onChangeNickname} />
           </div>
         </Label>
-        <Label id="password-label">
+        <Label htmlFor="password" id="password-label">
           <span>비밀번호</span>
           <div>
             <Input type="password" id="password" name="password" value={password} onChange={onChangePassword} />
           </div>
         </Label>
-        <Label id="password-check-label">
+        <Label htmlFor="confirmPassword" id="password-check-label">
           <span>비밀번호 확인</span>
           <div>
             <Input
               type="password"
               id="password-check"
               name="password-check"
-              value={passwordCheck}
-              onChange={onChangePasswordCheck}
+              value={confirmPassword}
+              onChange={onChangeConfirmPassword}
             />
           </div>
-          {/* {mismatchError && <Error>비밀번호가 일치하지 않습니다.</Error>}
-            {!nickname && <Error>닉네임을 입력해주세요.</Error>}
-            {signUpError && <Error>{signUpError}</Error>}
-            {signUpSuccess && <Success>회원가입되었습니다! 로그인해주세요.</Success>} */}
+          {mismatchError && <Error>비밀번호가 일치하지 않습니다.</Error>}
+          {!nickname && <Error>닉네임을 입력해주세요.</Error>}
         </Label>
         <Button type="submit">회원가입</Button>
+        <LinkContainer>
+          이미 회원이신가요?&nbsp;
+          <Link to="/login">로그인 하러가기</Link>
+        </LinkContainer>
       </Form>
-      <LinkContainer>
-        이미 회원이신가요?&nbsp;
-        <Link to="/login">로그인 하러가기</Link>
-      </LinkContainer>
     </div>
   );
 };
